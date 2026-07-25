6713051ad6790d08acf26a0b2f8196416d35a8bb
asutosh <ashutoshpastor123@gmail.com>
2026-07-25 19:37:25 +0530

    added space btw in message

--- commands.py
+++ commands.py
@@ -6,10 +6,10 @@ from excel_reader import get_day_menu
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "👋 Welcome to Mess Reminder Bot!\n\n"
-        "Available Commands:\n"
-        "/today\n"
-        "/tomorrow\n"
+        "👋 welcome to boku's kitchen \n\n"
+        "Available Commands:\n\n"
+        "/today\n\n"
+        "/tomorrow\n\n"
         "/week"
     )
 
